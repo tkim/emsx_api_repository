215687b9bc3a81fd5e6b374e04b50d462a47bca2
Terrence Kim <terrence.kim@gmail.com>
2017-11-21 16:27:50 -0500

    Update CreateOrderAndRouteWithStrat.java

--- EMSXFullSet_Java/CreateOrderAndRouteWithStrat.java
+++ EMSXFullSet_Java/CreateOrderAndRouteWithStrat.java
@@ -175,7 +175,7 @@ public class CreateOrderAndRouteWithStrat {
             	    //request.set("EMSX_CUSTOM_NOTE4", "Note4");
             	    //request.set("EMSX_CUSTOM_NOTE5", "Note5");
             	    //request.set("EMSX_EXCHANGE_DESTINATION", "ExchDest");
-            	    //request.set("EMSX_EXEC_INSTRUCTIONS", "AnyInst");
+            	    //request.set("EMSX_EXEC_INSTRUCTION", "Drop down values from EMSX Ticket");
             	    //request.set("EMSX_GET_WARNINGS", "0");
             	    //request.set("EMSX_GTD_DATE", "20170105");
             	    //request.set("EMSX_INVESTOR_ID", "InvID");
